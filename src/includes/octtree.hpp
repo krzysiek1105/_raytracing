3789a360e066731f3f6ff2ba693675898bba9608
Krzysiek1105 <krzysiek1105@opoczta.pl>
2018-10-28 21:24:06 +0100

    Octtree improvements

--- src/includes/octtree.hpp
+++ src/includes/octtree.hpp
@@ -4,16 +4,21 @@
 #include <vector>
 #include <array>
 
+#define MAX_RECURSION_LEVEL 4
+#define MAX_TRIANGLES_IN_NODE 16
+
 struct Node
 {
     AABB aabb;
     std::vector<Triangle> triangles;
+    std::vector<Node> nodes;
+    void split();
 };
 
 class Octtree
 {
     public:
-    std::array<Node, 8> nodes;
+    Node root;
     Octtree();
     Octtree(std::vector<Triangle> &triangles);
 };
--- src/includes/trace.hpp
+++ src/includes/trace.hpp
@@ -3,12 +3,24 @@
 #include "triangle.hpp"
 #include "octtree.hpp"
 
+void find(std::vector<Node *> &nodes, Node &current, Ray ray)
+{
+    if(current.nodes.size() == 0)
+        return;
+    if(current.aabb.ray_intersection(ray))
+        nodes.push_back(&current);
+    for(int i = 0; i < 8; i++)
+    {
+        if(current.nodes[i].aabb.ray_intersection(ray))
+            nodes.push_back(&current.nodes[i]);
+        find(nodes, current.nodes[i], ray);
+    }
+}
+
 Triangle *hit_info(Ray ray, Octtree &octtree, double &t)
 {
     std::vector<Node *> nodes;
-    for(Node &node : octtree.nodes)
-        if(node.aabb.ray_intersection(ray))
-            nodes.push_back(&node);
+    find(nodes, octtree.root, ray);
 
     double t_min = INFINITY;
     Triangle *hit = nullptr;
